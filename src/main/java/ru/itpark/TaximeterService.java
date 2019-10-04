547abfddb1ded7d19e54407444d83815cf5ea7c1
dmitry.novikov <student@ya.ru>
2019-10-04 20:22:59 +0300

    Final version service

--- src/main/java/ru/itpark/TaximeterService.java
+++ src/main/java/ru/itpark/TaximeterService.java
@@ -1,7 +1,20 @@
 package ru.itpark;
 
 public class TaximeterService {
-    int costOrder(int distance){
-        return 100;
+    int costOrder(int distance) {
+        int costLanding = 60;
+        int costPerKilometer = 20;
+        int discountPercent = 5;
+        int minBorderForDiscount = 1_000;
+        int maxDiscountCost = 100;
+        int generalCost = costLanding + distance * costPerKilometer;
+        if (generalCost > minBorderForDiscount){
+            int discountCost = generalCost * discountPercent / 100;
+            if(discountCost > maxDiscountCost){
+                discountCost = maxDiscountCost;
+            }
+            generalCost = generalCost - discountCost;
+        }
+        return generalCost;
     }
 }
